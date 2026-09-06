5ffb570126085f7f5029c62c8d0690cf429ec261
Arya Sarkar <arya.abct@gmail.com>
2026-09-06 20:17:46 +0000

    added objwct model

--- usr/include/abct/AbcTRuntime.hpp
+++ usr/include/abct/AbcTRuntime.hpp
@@ -1,2 +1,13 @@
+#pragma once
 #include <abct/AbcTTypes.hpp>
 #include <abct/AbcTBuiltIn.hpp>
+
+#include <format>
+#include <string>
+
+class Object {
+public:
+    std::string __str__() const {
+        return std::format("<Object at {}>", static_cast<const void*>(this));
+    }
+};
